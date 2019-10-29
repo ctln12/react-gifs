d3ac899ebc7efaee5aa0a2b3232f3f996d339548
Cataline Baert <snuf_kin@yahoo.com>
2019-10-29 10:05:42 +0100

    define new component

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <p>Hello from the app component!</p>
+    );
+  }
+}
+
+export default App;
