2cd8310ae97dbad7d8d08fd7d8eca67d29b90fb5
Cataline Baert <snuf_kin@yahoo.com>
2019-10-29 10:30:56 +0100

    change class into a function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="left-scene">
-          <SearchBar />
-          <div className="selected-gif">
-            <Gif />
-          </div>
-        </div>
-        <div className="right-scene">
-          <GifList />
+const App = () => {
+  return (
+    <div className="app">
+      <div className="left-scene">
+        <SearchBar />
+        <div className="selected-gif">
+          <Gif />
         </div>
       </div>
-    );
-  }
-}
+      <div className="right-scene">
+        <GifList />
+      </div>
+    </div>
+  );
+};
 
 export default App;
