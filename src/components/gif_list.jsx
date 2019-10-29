f56046f07f3848c1bbed1709654d9173cb323147
Cataline Baert <snuf_kin@yahoo.com>
2019-10-29 12:18:39 +0100

    refactor component

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import Gif from './gif';
+
 class GifList extends Component {
   render() {
     const { gifs } = this.props;
     return (
       <div className="gift-list">
         {gifs.map((gif) => {
-          return <img className="gif" key={gif.id} src={`http://giphygifs.s3.amazonaws.com/media/${gif.id}/giphy.gif`} alt="gif" />
+          return <Gif id={gif.id} key={gif.id} sourceId={gif.id} />
         })}
       </div>
     );
