1444b48e6acb5f31633950983e2b57d0138cd81f
Cataline Baert <snuf_kin@yahoo.com>
2019-10-29 12:17:56 +0100

    add giphy api key, search function and modify Gif props

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="left-scene">
-        <SearchBar />
-        <div className="selected-gif">
-          <Gif />
+const GIPHY_API_KEY = 'r655A3RW3xJPwN1GWesC4cAs7mYG8juv';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGifId: null
+    };
+  }
+
+  search = (query) => {
+    giphy({ apiKey: GIPHY_API_KEY, https: true })
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (error, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  };
+
+  render () {
+    const { selectedGifId, gifs } = this.state;
+    return (
+      <div className="app">
+
+        <div className="left-scene">
+
+          <SearchBar search={this.search} />
+          <div className="selected-gif">
+            <Gif sourceId={selectedGifId} />
+          </div>
+
         </div>
+
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
+
       </div>
-      <div className="right-scene">
-        <GifList />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
