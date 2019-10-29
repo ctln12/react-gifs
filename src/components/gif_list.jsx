b7f2ad81833c53b7bf2b121e6cad7e013ebe1f47
Cataline Baert <snuf_kin@yahoo.com>
2019-10-29 13:25:10 +0100

    fix lint errors

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,14 +1,18 @@
+/* eslint react/prefer-stateless-function: off */
+
 import React, { Component } from 'react';
 
 import Gif from './gif';
 
 class GifList extends Component {
   render() {
-    const { gifs } = this.props;
+    const { gifs, selectGif } = this.props;
     return (
       <div className="gif-list">
         {gifs.map((gif) => {
-          return <Gif id={gif.id} key={gif.id} sourceId={gif.id} />
+          return (
+            <Gif id={gif.id} key={gif.id} sourceId={gif.id} selectGif={selectGif} />
+          );
         })}
       </div>
     );
