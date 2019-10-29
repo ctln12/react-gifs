48c291606cba227002ac34c9664df62e1ccc9eb4
Cataline Baert <snuf_kin@yahoo.com>
2019-10-29 13:24:44 +0100

    fix lint errors and refactor

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,11 +1,18 @@
+/* eslint jsx-a11y/click-events-have-key-events: off */
+/* eslint jsx-a11y/no-noninteractive-element-interactions: off */
+/* eslint react/destructuring-assignment: off */
+
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  handleClick = () => {
+    this.props.selectGif(this.props.sourceId);
+  };
+
   render() {
-    const { sourceId } = this.props;
-    const src = `http://giphygifs.s3.amazonaws.com/media/${sourceId}/giphy.gif`;
+    const src = `http://giphygifs.s3.amazonaws.com/media/${this.props.sourceId}/giphy.gif`;
     return (
-        <img className="gif" src={src} />
+      <img src={src} className="gif" onClick={this.handleClick} alt="gif" />
     );
   }
 }
